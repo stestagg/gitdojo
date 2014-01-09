dae3fcb2e39a8017723c7d4219a30f4c9d583f53
Steve Stagg <stestagg@gmail.com>
2014-01-09 21:47:24 +0000

    Indentation

--- main_tests.py
+++ main_tests.py
@@ -2,8 +2,8 @@ import unittest
 
 class MainTests(unittest.TestCase):
 
-  def test_stuff(self):
-    self.assertEqual(1+1, 2)
+    def test_stuff(self):
+        self.assertEqual(1+1, 2)
 
 if __name__ == "__main__":
-  unittest.main()
+    unittest.main()
