38d85c8a977bd53daef8ea4c496a3955a922b457
Steve Stagg <stestagg@gmail.com>
2014-01-09 20:29:05 +0000

    Add script file

---
+++ main.py
@@ -0,0 +1,18 @@
+import subprocess
+import sys
+
+
+def get_commit_hashes(directory):
+  pass
+
+
+def get_hash_info(hash, directory):
+  pass
+
+
+def get_files_and_change_commits(directory):
+  pass
+
+
+if __name__ == "__main__":
+  sys.exit(main)
